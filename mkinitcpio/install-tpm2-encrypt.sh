135b4fad48203dbe8e2a8e58ed817d4ac48eaaa6
Rowan Moul <rowanmoul@users.noreply.github.com>
2020-02-29 09:11:40 -0700

    Remove direct handle arguments. File only handles. Require tpm_efi_part argument. Reformat Help text to width of 80 chars.

--- mkinitcpio/install-tpm2-encrypt.sh
+++ mkinitcpio/install-tpm2-encrypt.sh
@@ -23,31 +23,56 @@ build() {
 
 help() {
     cat <<HELPEOF
-This hook is an extension for the 'encrypt' hook that allows for an encrypted root device that can be unlocked with a TPM 2.0 device.
-It MUST come before the 'encrypt' hook, and will generate a keyfile in the default location for the 'encrypt' hook to use.
+This hook is an extension for the 'encrypt' hook that allows for an encrypted
+root device that can be unlocked with a TPM 2.0 device.
+It MUST come before the 'encrypt' hook, and will generate a keyfile in the
+default location for the 'encrypt' hook to use.
 
-Users should specify the device to be unlocked according the requirements of the 'encrypt' hook.
+Users should specify the device to be unlocked according to
+the requirements of the 'encrypt' hook.
 
-For unlocking via TPM 2.0, this hook makes use of LUKS passphrase sealed in a TPM object that is restricted with a policy that requires a second policy signed with an authorization key. Intial setup is too lengthy for this help text. Please refer to the readme in this repo: https://github.com/rowanmoul/ArchSecureDualBoot
+For unlocking via TPM 2.0, this hook makes use of LUKS passphrase sealed in a
+TPM object that is restricted with a policy that requires a second policy signed
+with an authorization key. Intial setup is too lengthy for this help text.
+Please refer to the readme in this repo:
+https://github.com/rowanmoul/ArchSecureDualBoot
 
-To specify the sealed TPM object's location in the TPM NVRAM, 'tpm_sealed_key=handle' can be specified on
-the kernel command line, where 'handle' is either a hexidecimal value indicating the NVRAM location directly (in the form 0x81000000), or the path to a handle file on the EFI system partition.
-If this argument is not specified the hook will look for a file called 'sealed-passphrase.handle' in /EFI/arch/tpm2-encrypt on the EFI system partition.
+required kernel command line arguments:
 
-To specify the policy authorization key's location in the TPM NVRAM, 'tpm_policy_auth=name:handle' can be specified on the kernel command line.
-'name' is the name of the key in the TPM, (typically a sha256 digest), or the path to a name file in on the EFI system partition.
-'handle' is as above for the sealed key except is specifies the location of the policy authorization key.
-If this argument is not specified, the hook will look for files called 'policy-authorization-key.name' and 'policy-authorization-key.handle' in /EFI/arch/tpm2-encrypt on the EFI system partition.
+tpm_efi_part=device
+    - Must be specified on the kernel command line in order to access
+      the EFI system partition.
+    - 'device' can be a location in /dev or a uuid etc.
+    - Refer to documentation for specifying the 'root=device' argument for
+      formatting details, but uuid is recommended.
+    - Technicaly you could put any unencrypted partition here.
+      EFI is just the simplest and nearly guaranteed to be available.
 
-In order to access the EFI system partition, 'efi_part=device' must be specified on the kernel command line, where 'device' can be a location in /dev or a uuid etc. Refer to documentation for specifying the `root=device` argument for formatting details.
+optional kernel command line arguments:
 
-If the handle of a TPM entity cannot be found, or if the TPM fails to unseal the key, you will be prompted for the passphrase.
-This means you must have a keyboard available to input it, and you may need
-the keymap hook as well to ensure that the keyboard is using the layout you
-expect.
+tpm_sealed_key=handle
+    - Specifies the sealed TPM object's location in the TPM NVRAM.
+    - 'handle' is the path to a handle file on the given partition.
+    - If this argument is not specified the hook will look for a file called
+    'sealed-passphrase.handle' in /EFI/arch/tpm2-encrypt on the given partition.
 
-In the case that the TPM fails to unseal the key, a list of PCR values that changed since the last seal will be shown,
-and you will be asked if you want to re-seal on the new PCR values after unlocking the volume with a password. The previous values are signed and verified with the policy authorization key to maintain integrity and trust.
+'tpm_policy_auth=name:handle'
+    - Specifies the policy auth key's name and location in the TPM NVRAM.
+    - 'name' is the path to a name file in on the given partition.
+    - 'handle' is the path to a handle file on the given partition.
+    - If this argument is not specified, the hook will look for files called
+      'policy-authorization-key.name' and 'policy-authorization-key.handle'
+      in /EFI/arch/tpm2-encrypt on the EFI system partition.
+
+If the handle of a TPM entity cannot be found, or if the TPM fails
+to unseal the key, you will be prompted for the passphrase.
+This means you must have a keyboard available to input it.
+
+In the case that the TPM fails to unseal the key, a list of PCR values that
+changed since the last seal will be shown, and you will be asked if you want
+to re-seal on the new PCR values after unlocking the volume with a password.
+The previous values are signed and verified with the policy authorization key
+to maintain integrity and trust.
 HELPEOF
 }
 
